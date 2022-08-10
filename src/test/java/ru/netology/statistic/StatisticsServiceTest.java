2281b01057757d9a3501e2be9be949577b1fcb2b
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2022-08-10 18:57:49 +0300

    all

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        StatisticsService service = new StatisticsService();
+
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
