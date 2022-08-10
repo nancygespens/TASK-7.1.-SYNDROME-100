a7946d70a497491ccdcf769655aea1523c4cf0cc
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2022-08-10 19:26:15 +0300

    all

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,17 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldFindMaxInTheMiddle() {
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
+
+        StatisticsService service = new StatisticsService();
+
+        long expected = 30;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
